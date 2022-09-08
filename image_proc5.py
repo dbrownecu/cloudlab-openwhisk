c7f0682e65a01c3930437bce1fb0853262235689
dbrowne <dwight@dwightjbrowne.com>
2022-09-07 18:45:57 -0700

    more fixes

--- image_proc5.py
+++ image_proc5.py
@@ -4,6 +4,7 @@ from cloudant.client import CouchDB
 import time
 import cv2
 from numpy import asarray
+import numpy as np
 
 K_DBNAME = "frshimg"
 K_DB2NAME = "resizeimg"
@@ -47,14 +48,14 @@ def write_file(pth, fn, dta):
     return ("wrote: {}".format(fullpth))
 
 
-def preprocess_image(time_dict, img_arr):
+def preprocess_image(im_arr,time_dict):
     start = time.time()
-    img_array = asarray(img_arr)
+    im_arr = im_arr +b'\x00\x00\x00\x00\xae'  # round the buffer to a multiple of 32
+    img_array = np.frombuffer(im_arr, np.float32)
     end = time.time() - start
-
-    time_dict["asarray of img"] = (start, end)
+    time_dict["array of img"] = (start, end)
     start = time.time()
-    img2 = cv2.resize(img_array, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    img2 = cv2.resize(img_array, none,  fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
     end = time.time() - start
     time_dict["cv2.resize"] = (start, end)
     return img2
@@ -109,6 +110,7 @@ def main(args):
     db1 = args.get("db1", K_DBNAME)
     db2 = args.get("db2", K_DB2NAME)
     count = args.get("count", 1)
+    print("THIS IS IS\n")
     recval = test_count(user, passwd, url, count, db1, db2)
     return {
         "statusCode": 0,
@@ -116,3 +118,7 @@ def main(args):
             "label": recval,
         })),
     }
+
+#if __name__ == "__main__":
+#    test_count("admin","SO8J3E7p1fblnTVR3M2h","http://10.102.18.189:5984/",1, K_DBNAME,K_DB2NAME)
+
