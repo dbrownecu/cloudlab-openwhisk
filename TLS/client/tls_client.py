a9996a51c467724108d2d58aa45a2b2385e058fc
dbrowne <dwight@dwightjbrowne.com>
2022-10-27 16:21:57 -0700

    Resolved cert issue.

--- TLS/client/tls_client.py
+++ TLS/client/tls_client.py
@@ -2,7 +2,8 @@ import socket
 import ssl
 import time
 import json
-CERTSTRING="""-----BEGIN CERTIFICATE-----
+import os
+CLIENTCERT="""-----BEGIN CERTIFICATE-----
 MIIDzzCCArcCFECe1578Mv9NE/jw7ypS5TaJ0aUlMA0GCSqGSIb3DQEBCwUAMIGj
 MQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2Fu
 IEZyYW5jaXNjbzETMBEGA1UECgwKQ1UgQm91bGRlcjEUMBIGA1UECwwLU3lzdGVt
@@ -27,65 +28,40 @@ EZZdP4LbqUQ38Npik2SJtj4iSA==
 -----END CERTIFICATE-----"""
 
 
-KEYSTRING="""-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCeffeDaE7c6gKX
-d4UbgFIdcT9ZOnMWirx787JI4hfTw3g6ks8gydxnL0lIFwcRy9KGhaiyGt08GKhQ
-0ReXfaBDr6KrxNRROP4bp3WK8fzM/rFnIrTEX8Efwj6Fu1+XBNpSJjMwd5H7LcL0
-TNSG350s75/bZO7hRo3pFjTIbdZiM48HLxaqVklQF1K1gEQiVXL3L8wsad49q/4d
-G7kWGRpSXyeRsvh2N1A+1v+Wx2iIwm6icFkqi3AL7DskdBYJRdsbbbHACYI7sCgj
-TqI8gqT9J1q6A06IZZ4aSaMK6rwXHxxNwU0UY1KcngeWo0SS4ABw8w6qiMXarhrQ
-ONmAMiDjAgMBAAECggEAO48y2Fr2Z+Y4mxr7Fl6efRn1qF5iqXHxatMliGYOdjHa
-mIha9gNlpBENBN5CddmWf57yyht2UAMkHVq19uPqugTiqJILB4rXUyUW8uQFEDQW
-Dp/oNnOVa43fWPoU7feQ45YfXnNQlIeZ35yPsS+PLTfPZu8DJf0RjyodI8TC75vA
-v7THTdeMmuhf+Ivx9yiTvOfjuFHvc7A6NDNJpOgF+rbULNXnxIci5ttdDK7uGzpF
-Wvk3Ha9SI474ewa83z0vhqbcNFUfncicwVMtdoCFHcm9Qb4d8IBrd0P/uug2wncs
-hs0ER9iw4bXamjcC35ucxoh9snqRhq2K07srS2Z2gQKBgQDXyjnNAPGQe060bmmS
-2CIXRvW3nWv75X0w3i0ogH0nyt53ezWlbDsaiMEQIZovvoB0bT34sO7tFHj9YYSk
-B6mWK2eCKZmjx//LRrdoBGuNBkoJLCZ8DB0IBVf4fEK7qX6O/bFC4BIVIgEo72Le
-fIce36gM7e3bksY+y/BNYIp4kQKBgQC8BnlkvmcRCR79JpMx0aM3X/E6WfYxyNKk
-yQpx5bbG98xfYxjyd85QF9BKw5giG3T9Ba/sO4JRn9UTVJf2/vg2baZxKmbYzv5L
-4J/jbUpSkPeQjR7k4KvrB8TlfFIVMJHNUspyWZpz4WiPfJ3bOpP14JIFUr43JZzr
-Q0vCUMhcMwKBgQCebBb3TIQMYJqtmInlhsuwbyYiQW5vplG8uMBKhdpchnHBIjGG
-WukhV9j1cNXzy1YfI7xlgfBHGcqhqyBoFEAKwZ7iTow/U6uVnzszIFudU0qzZThz
-xajipJmrEf8kKxGBBPtIb5yW6zoSxXy+fonsvqJvq5X+p8jnfW3UQNYoIQKBgH3T
-U09gv7Y9xVghhWVUZgY7lUG4TDHas2QFCZFY6WzTDRhUR7CTDiS5GvG9XMSOXUap
-lKXg2P/olv29oYhv7gj2bejTOEokkzaR1k2qqI8CZjl8xszhsQqyg0gTXxbBDwE5
-QzMs1Vhw4rkK6tR3qeZj5zPawGJFMHKb8UnTJ5HTAoGBAMdc0sAcy6x4G/cj7F+b
-0bDg9MaWD168pjuYWkl6sXsRfJiy9OE3h6lOW028s/UW8rv2dTsuodX41/hQQ0Ey
-aRmPrkWzdVInXsZNgxSm+TTQHNwT+2uflvsUe92/0fOfJo09V18y3yOqt3Iby/lc
-LHL1JBlpleFmBK6ZZ72jRilx
------END PRIVATE KEY-----"""
 
-KEYFILE ="/tmp/clientKey"
+
 CERTFILE = "/tmp/clientCert"
 
 def genFiles():
-    fn = open(KEYFILE,"w")
-    fn.write(KEYSTRING)
-    fn.close()
     fn = open(CERTFILE,"w")
-    fn.write(CERTSTRING)
+    fn.write(CLIENTCERT)
     fn.close()
+    os.chmod(CERTFILE, 0o700)
 
 
+def connectSendLoop(host, port, interval, delay ):
 
-def connectSendLoop(host, interval, delay ):
-
+    genFiles()
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.load_verify_locations("Needcabundle.pem")
+    context.load_verify_locations(CERTFILE)
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+    with socket.create_connection(('127.0.0.1', port)) as  sock:
         with context.wrap_socket(sock, server_hostname=host) as ssock:
+            if ssock:
+                print("soket {}".format(ssock))
+                for i in range(interval):
+                    t = time.time()
+                    stamp = "{}: {} this is a test\n".format(i, t)
+                    print(stamp)
+                    ssock.send(stamp.encode("utf-8"))
+                    time.sleep(delay)
+            else:
+                print("Socket problems {}\n".format(ssock))
 
-            for i in range(interval):
-                t = time.time()
-                stamp = "{}: {} this is a test\n".format(i, t)
-                ssock.send(stamp.encode("utf-8"))
-                time.sleep(delay)
-
-
-def commonfunc(host,interval,delay):
-    connectSendLoop(host,interval,delay)
+def commonfunc(host,port,interval,delay):
+    connectSendLoop(host,port, interval,delay)
     return {
         "statusCode" :0,
         "body": json.dumps(({
@@ -100,13 +76,14 @@ def main(args):
     interval = args.get("interval",10)
     delay = args.get("delay",0)
 
-    retval = commonfunc(host,interval, delay)
+    retval = commonfunc(host,port, interval, delay)
     return retval
 
 
 if __name__ == '__main__':
     host = "localhost"
+    port = 8443
     interval = 100
     delay = 0
 
-    retval = commonfunc(host,interval, delay)
+    retval = commonfunc(host,port,interval, delay)
--- TLS/server/tls_server.py
+++ TLS/server/tls_server.py
@@ -1,11 +1,15 @@
 import socket
 import ssl
 
-
+SERVERPEM="keys/server.crt"
+PRIVATEKEY="keys/server.key"
 def listener():
     buff = []
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain("NEEDcertchain.pem","privatekey")
+    context.load_cert_chain(SERVERPEM,PRIVATEKEY)
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
         sock.bind(('127.0.0.1',8443))
@@ -19,3 +23,7 @@ def listener():
                 if not data:
                     break
                 print("received: {}".format(data.decode('utf-8')))
+
+
+if __name__ == '__main__':
+    listener()
